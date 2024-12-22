93f1e3b9189a5522a1346323c7fbf3beccfe9639
Karamjeet Singh <singhkaramjeetapple.com>
2024-12-21 17:11:33 -0800

    checkUser done

---
+++ src/actions/user.ts
@@ -0,0 +1,26 @@
+import { db } from "@/lib/prisma";
+import { currentUser } from "@clerk/nextjs/server";
+
+export async function checkUser() {
+  try {
+    const clerkUser = await currentUser();
+
+    if (!clerkUser) throw new Error("Unauthorized");
+
+    const user = await db.user.findUnique({
+      where: { clerkUserId: clerkUser.id },
+    });
+    if (user) return;
+
+    await db.user.create({
+      data: {
+        clerkUserId: clerkUser.id,
+        name: clerkUser.fullName,
+        email: clerkUser.emailAddresses.at(0)?.emailAddress as string,
+        imageUrl: clerkUser.imageUrl,
+      },
+    });
+  } catch (error) {
+    console.error("Something went wrong while checkUser in DB", error);
+  }
+}
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,10 @@ import Link from "next/link";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { LayoutDashboard, PenBox } from "lucide-react";
+import { checkUser } from "@/actions/user";
 
-const Header = () => {
+const Header = async () => {
+  await checkUser();
   return (
     <header className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-10 border-b">
       <nav className="container mx-auto p-4 flex justify-between items-center">

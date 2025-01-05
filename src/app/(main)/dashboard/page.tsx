7239aff0e44908467659e2f2f2f28036df7ed006
Karamjeet Singh <singhkaramjeetapple.com>
2025-01-05 14:46:04 -0800

    production dashboard bug fix

--- src/app/(main)/dashboard/page.tsx
+++ src/app/(main)/dashboard/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = "force-dynamic";
+export const dynamic = "auto";
 
 import CreateAccountDrawer from "@/components/CreateAccountDrawer";
 import { Card } from "@/components/ui/card";
